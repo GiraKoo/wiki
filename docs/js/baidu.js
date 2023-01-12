017730a7596ab6285c5195f09ef029e129a7c474
GiraKoo <girakoo@163.com>
2023-01-12 10:22:57 +0800

    Add baidu.js

---
+++ docs/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?969a35e8a09597d9fb1313032ebb8589";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
