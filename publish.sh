0712517b1061072b42fdaeedb8c98d83c2a6d3f8
GiraKoo <girakoo@163.com>
2023-03-09 17:11:01 +0800

    docs: update bash shell

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+web_path=/www/wwwroot/wiki.girakoo
+
+git clean -fxd
+git reset HEAD --hard
+git pull
+
+mkdocs build
+if [ -d ${web_path} ]; then
+	sudo rm ${web_path}/*
+	cp site/* ${web_path}/ -rf
+	sudo chown www:www ${web_path} -R
+fi
